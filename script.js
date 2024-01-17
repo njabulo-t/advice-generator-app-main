f45fa0143a8ad6f1c95851616797fb4e044212e7
Njabulo <njabulonkosi59.com>
2024-01-17 17:42:56 +0200

    Javascript API set up completed

---
+++ script.js
@@ -0,0 +1,36 @@
+
+function getApi() {
+let apiUrl = `https://api.adviceslip.com/advice`;
+console.log(apiUrl)
+
+axios.get(apiUrl)
+.then(response => {
+    displayAdvice(response.data.slip.advice);
+    displayAdviceId(response.data.slip.id)
+})
+.catch(error => {
+    console.error("Error fetching advice:", error);
+});
+}
+
+function displayAdviceId(id) {
+    let adviceTitle = document.querySelector("#advice-title");
+    adviceTitle.innerHTML = `advice #${id}`;
+}
+
+function displayAdvice(advice) {
+   
+    let adviceContent = document.querySelector("#advice-content");
+    adviceContent.textContent = advice;
+}
+
+function getAdvice() {
+    getApi();
+   
+    let adviceDice = document.querySelector("#advice-dice");
+adviceDice.addEventListener("click", getApi);
+}
+getAdvice();
+
+
+
